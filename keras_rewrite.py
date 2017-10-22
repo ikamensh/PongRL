59d1f7e4701aa9ae1bdc818af4ae49c30908c28a
Ilya <ikamenshchikov@gmail.com>
2017-10-22 17:44:57 +0200

    lists to np.array

--- keras_rewrite.py
+++ keras_rewrite.py
@@ -51,18 +51,18 @@ episode_number = 0
 
 while True:
     if render: env.render()
+    x_train_episode, y_train_episode = [], []
 
     # preprocess the observation, set input to network to be difference image
     cur_x = prepro(observation)
     x = cur_x - prev_x if prev_x is not None else np.zeros(shape=(1,D,D,1))
     prev_x = cur_x
+    x_train_episode.append(x)
 
     # forward the policy network and sample an action from the returned probability
     up_prob = model.predict_proba(x.reshape(1,80,80,1), verbose=0)
     action = 2 if np.random.uniform() < up_prob else 3  # roll the dice!
     y = 1 if action == 2 else 0  # a "fake label"
-    x_train_episode, y_train_episode = [], []
-    x_train_episode.append(x)
     y_train_episode.append(y)
 
     # step the environment and get new measurements
